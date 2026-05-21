717fb0f104be8ba37406a19db443e872d1890be1
thavidaboinabhavyasree <thavidaboinabhavyasree@gmail.com>
2026-05-21 10:24:08 +0530

    updated backend url

--- ATP_24EG105K52-main/WEEK-6/mern-app-emps/frontend/vite.config.js
+++ ATP_24EG105K52-main/WEEK-6/mern-app-emps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://k52-week6.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/emp-api')
       }
